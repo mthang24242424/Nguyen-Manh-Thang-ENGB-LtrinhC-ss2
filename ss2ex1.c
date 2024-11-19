7566515ae7823d50d75f0db34dd7bf09ba34ecd8
mthang24242424 <wunkevan@gmail.com>
2024-11-19 18:36:45 +0700

    ss2 ex1

---
+++ ss2ex1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	
+	// khai bao bien kieu du lieu int
+	int age;
+	
+	// khai bao bien kieu du lieu float
+	float pi;
+	
+	//khoi tao gia tri cho bien 
+	age = 18;
+	pi = 3.14;
+	
+	// truy xuat gia tri cua bien
+	printf("nam nay toi %d\n",age);
+	printf("so pi la %.2f",pi);
+	
+	return 0;
+	
+}
