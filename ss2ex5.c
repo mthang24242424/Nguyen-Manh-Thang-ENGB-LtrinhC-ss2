17356f99826ff683abd4a8fc2b0c0d7285737d59
mthang24242424 <wunkevan@gmail.com>
2024-11-19 18:37:48 +0700

    ss2 ex5

---
+++ ss2ex5.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+
+int main(){
+	
+	int lengthRectangle = 5;
+	int widthRectangle = 3; 
+	int perimeter = (lengthRectangle + widthRectangle)* 2 ;
+	int area = lengthRectangle * widthRectangle;
+	printf("chu vi la %d\n",perimeter);
+	printf("dien tich la %d",area);
+	return 0; 
+} 
