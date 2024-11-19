cde6b4ecbdb202dee6bafc63e0dcf6249c6561fd
mthang24242424 <wunkevan@gmail.com>
2024-11-19 18:37:32 +0700

    ss2 ex4

---
+++ ss2ex4.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+
+int main(){
+	
+	int lengthSquare = 5;
+	int perimeter =  lengthSquare * 4;
+	int area = lengthSquare * lengthSquare;
+	printf("chu vi la %d\n",perimeter);
+	printf("dien tich la %d",area);
+	return 0; 
+} 
