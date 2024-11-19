a800d8eb88aabbe58363ff1e7bef2593737c1cb2
mthang24242424 <wunkevan@gmail.com>
2024-11-19 18:38:02 +0700

    ss2 ex6

---
+++ ss2ex6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	float pi = 3.14;
+	int banKinh = 8;
+	float chuVi = 4*pi*banKinh;
+	float dienTich = pi*banKinh*banKinh; 
+	printf("chu vi hinh tron bang %.2f\n",chuVi);
+	printf("dien tich hinh tron bang %.2f",dienTich);
+	return 0; 
+} 
