b0a450ca24a5bc50713d50106b86fa7a656081d4
mthang24242424 <wunkevan@gmail.com>
2024-11-19 18:37:16 +0700

    ss2 ex3

---
+++ ss2ex3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a = 5;
+	int b = 10;
+	int tong = a + b;
+	printf("tong cua 2 so la %d\n",tong);
+	int hieu = a - b;
+	int tich = a * b;
+	int thuong = a / b;
+	 	printf("hieu cua 2 so la %d\n",hieu);
+	printf("tich cua 2 so la %d\n",tich);
+	printf("thuong cua 2 so la %d\n",thuong);
+	
+
+	return 0;
+}
